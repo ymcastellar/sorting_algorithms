2c9364220baa2151c13f1b362b94e9f04e7b7296
ymcastellar <yoycas@hotmail.com>
2020-06-12 18:11:08 -0500

    added files for task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+*bubble_sort - sort array with bubble method
+*@array: list to sort
+*@size: size of the list
+*Return: void
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+    int aux;
+
+    if (array && size)
+    {
+        for (size_t i = 0; i < size; i++)
+        {
+            for(size_t j = 0; j < size - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                { 
+                    aux = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = aux;
+                    print_array(array, size);
+                }
+            }
+        }
+    }
+    else
+    {
+        return;
+    }
+}
